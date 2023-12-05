a8e792322d8d3a795eeeec19fde29c58bb528032
Cristina Palmisani <cristina.palmisani@outlook.it>
2023-12-05 13:36:03 +0100

    Esercizio 1 completato

---
+++ src/Es1/Main.java
@@ -0,0 +1,39 @@
+package Es1;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+      int numberOfWord = 0;
+      String word = "";
+
+      while (numberOfWord <= 0){
+          System.out.println("Insert the number of words you want!");
+          try {
+              numberOfWord = Integer.parseInt(input.nextLine());
+          } catch (NumberFormatException ex){
+              System.err.println("Please insert a number");
+          }
+      }
+
+        HashSet<String> wordSet = new HashSet<>();
+        HashSet<String> duplicateSet = new HashSet<>();
+        for (int i = 0; i < numberOfWord ; i++) {
+            System.out.println("Insert string number" + (i + 1));
+            word = input.nextLine();
+            if (!wordSet.add(word)){
+                System.out.println(word + "already inserted");
+                duplicateSet.add(word);
+            }
+        }
+        System.out.println("duplicate words: " + duplicateSet);
+        System.out.println("There are: " + wordSet.size());
+        System.out.println("this is the unique words: " + wordSet);
+
+
+
+    }
+}
